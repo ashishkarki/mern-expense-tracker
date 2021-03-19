8789defd9b4a8b19ae72c583f3af20f0ad10c2f3
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-03-19 10:01:01 +0545

    adding routes and controllers folders to separate concerns for transactions

---
+++ routes/transactions.js
@@ -0,0 +1,21 @@
+
+const express = require('express')
+const router = express.Router()
+const {
+    getTransactions,
+    addTransaction,
+    deleteTransaction,
+}
+    = require('../controllers/transactions')
+
+// router.get('/', (req, res) => res.send('hello from transactions!!'))
+router
+    .route('/')
+    .get(getTransactions)
+    .post(addTransaction)
+
+router
+    .route('/:id')
+    .delete(deleteTransaction)
+
+module.exports = router
